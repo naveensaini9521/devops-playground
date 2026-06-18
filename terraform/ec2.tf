736100a1350a18df36d2d90f5112249727c84ef4
naveen saini <naveensaini6378@gmail.com>
2026-06-18 09:32:42 +0530

    add dynamic inventory with group tag names

--- terraform/ec2.tf
+++ terraform/ec2.tf
@@ -1,22 +1,28 @@
-data "aws_ami" "ubuntu" {
+# Amazon Linux 2023 ARM64 AMI
+data "aws_ami" "amazon_linux" {
   most_recent = true
 
-  owners = ["099720109477"] # Canonical
+  owners = ["amazon"]
 
   filter {
     name   = "name"
-    values = ["ubuntu/images/hvm-ssd/ubuntu-jammy-22.04-amd64-server-*"]
+    values = ["al2023-ami-2023.*-x86_64"] # x86_64 architecture
   }
 
   filter {
     name   = "virtualization-type"
     values = ["hvm"]
   }
+
+  filter {
+    name   = "architecture"
+    values = ["x86_64"]
+  }
 }
 
 resource "aws_instance" "smart_voting_server" {
-  ami                    = data.aws_ami.ubuntu.id
-  instance_type          = var.instance_type
+  ami                    = data.aws_ami.amazon_linux.id
+  instance_type          = "t2.micro"
   key_name               = "devops-key"
   subnet_id              = aws_subnet.public.id
   vpc_security_group_ids = [aws_security_group.smart_voting_sg.id]
