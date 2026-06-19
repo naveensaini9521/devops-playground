e4ddd5b2a941a8ca73af6d5c1f83b2b51b61dd62
naveen saini <naveensaini6378@gmail.com>
2026-06-19 22:18:56 +0530

    add NAT for private network

---
+++ terraform/nat.tf
@@ -0,0 +1,12 @@
+# Elastic IP for NAT Gateway
+resource "aws_eip" "nat" {
+  domain = "vpc"
+  tags   = { Name = "smart-voting-nat-eip" }
+}
+
+# NAT Gateway 
+resource "aws_nat_gateway" "nat" {
+  allocation_id = aws_eip.nat.id
+  subnet_id     = aws_subnet.public[0].id
+  tags          = { Name = "smart-voting-nat" }
+}
