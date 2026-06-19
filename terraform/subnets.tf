e0c3d88bc7b81638d196d255d33e56925fabceb9
naveen saini <naveensaini6378@gmail.com>
2026-06-19 22:46:10 +0530

    add public & private subnets

--- terraform/subnets.tf
+++ terraform/subnets.tf
@@ -1,20 +1,20 @@
 resource "aws_subnet" "public" {
   vpc_id                  = aws_vpc.main.id
-  cidr_block              = "10.0.1.0/24"
-  availability_zone       = "ap-south-1b"
+  cidr_block              = cidrsubnet("10.0.0.0/16", 8, count.index)
+  availability_zone       = element(["ap-south-1a", "ap-south-1b"], count.index)
   map_public_ip_on_launch = true
 
   tags = {
-    Name = "public-subnet"
+    Name = "public-subnet-${count.index}"
   }
 }
 
 resource "aws_subnet" "private" {
   vpc_id            = aws_vpc.main.id
-  cidr_block        = "10.0.2.0/24"
-  availability_zone = "ap-south-1b"
+  cidr_block        = cidrsubnet("10.0.0.0/16", 8, count.index + 10)
+  availability_zone = element(["ap-south-1a", "ap-south-1b"], count.index)
 
   tags = {
-    Name = "private-subnet"
+    Name = "private-subnet-${count.index}"
   }
 }
