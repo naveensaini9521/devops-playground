1aea33c9d55de31d2b662f5c6ef40795fba310a7
naveen saini <naveensaini6378@gmail.com>
2026-06-21 22:11:40 +0530

    fix roues table

--- terraform/routes_table.tf
+++ terraform/routes_table.tf
@@ -1,5 +1,3 @@
-# PUBLIC ROUTE TABLE
-
 resource "aws_route_table" "public_rt" {
   vpc_id = aws_vpc.main.id
 
@@ -13,24 +11,24 @@ resource "aws_route_table" "public_rt" {
   }
 }
 
-# PUBLIC SUBNET ASSOCIATION
-
 resource "aws_route_table_association" "public_assoc" {
-  subnet_id      = aws_subnet.public.id
+  count          = 2
+  subnet_id      = aws_subnet.public[count.index].id
   route_table_id = aws_route_table.public_rt.id
 }
 
-# PRIVATE ROUTE TABLE (NO INTERNET ACCESS)
-
 resource "aws_route_table" "private_rt" {
   vpc_id = aws_vpc.main.id
+
+  route {
+    cidr_block     = "0.0.0.0/0"
+    nat_gateway_id = aws_nat_gateway.nat.id
+  }
 
   tags = {
     Name = "private-route-table"
   }
 }
-
-# PRIVATE SUBNET ASSOCIATION
 
 resource "aws_route_table_association" "private_assoc" {
   subnet_id      = aws_subnet.private.id
