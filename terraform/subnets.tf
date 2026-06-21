dd2dddf5bf91c199829447bc507040c32945ea9b
naveen saini <naveensaini6378@gmail.com>
2026-06-21 22:10:00 +0530

    integrate private & public subnet

--- terraform/subnets.tf
+++ terraform/subnets.tf
@@ -1,20 +1,17 @@
+# Public subnet (for ALB, bastion, app instances)
 resource "aws_subnet" "public" {
+  count                   = 2
   vpc_id                  = aws_vpc.main.id
   cidr_block              = cidrsubnet("10.0.0.0/16", 8, count.index)
   availability_zone       = element(["ap-south-1a", "ap-south-1b"], count.index)
   map_public_ip_on_launch = true
-
-  tags = {
-    Name = "public-subnet-${count.index}"
-  }
+  tags                    = { Name = "public-subnet-${count.index}" }
 }
 
+# Private subnet (for database instances)
 resource "aws_subnet" "private" {
   vpc_id            = aws_vpc.main.id
-  cidr_block        = cidrsubnet("10.0.0.0/16", 8, count.index + 10)
-  availability_zone = element(["ap-south-1a", "ap-south-1b"], count.index)
-
-  tags = {
-    Name = "private-subnet-${count.index}"
-  }
+  cidr_block        = "10.0.2.0/24"
+  availability_zone = "ap-south-1a"
+  tags              = { Name = "private-subnet" }
 }
