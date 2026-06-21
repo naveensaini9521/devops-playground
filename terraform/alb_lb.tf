c73870fca43cc5aa50bc36af14bea200d6fa0e53
naveen saini <naveensaini6378@gmail.com>
2026-06-21 22:08:00 +0530

    add ALB

---
+++ terraform/alb_lb.tf
@@ -0,0 +1,34 @@
+resource "aws_lb" "app" {
+  name               = "smart-voting-alb"
+  internal           = false
+  load_balancer_type = "application"
+  security_groups    = [aws_security_group.alb_sg.id]
+  subnets            = aws_subnet.public[*].id
+  tags               = { Name = "smart-voting-alb" }
+}
+
+# Target Group (unchanged)
+resource "aws_lb_target_group" "app" {
+  name     = "smart-voting-tg"
+  port     = 80
+  protocol = "HTTP"
+  vpc_id   = aws_vpc.main.id
+  health_check {
+    path                = "/"
+    interval            = 30
+    timeout             = 5
+    healthy_threshold   = 2
+    unhealthy_threshold = 2
+  }
+  tags = { Name = "smart-voting-tg" }
+}
+
+resource "aws_lb_listener" "http" {
+  load_balancer_arn = aws_lb.app.arn
+  port              = "80"
+  protocol          = "HTTP"
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.app.arn
+  }
+}
