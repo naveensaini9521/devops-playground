1ff4121d26e8d9d63400c6fbc7f547bbc9336754
naveen saini <naveensaini6378@gmail.com>
2026-06-22 09:58:16 +0530

    update load balancer

--- terraform/alb_lb.tf
+++ terraform/alb_lb.tf
@@ -4,15 +4,17 @@ resource "aws_lb" "app" {
   load_balancer_type = "application"
   security_groups    = [aws_security_group.alb_sg.id]
   subnets            = aws_subnet.public[*].id
-  tags               = { Name = "smart-voting-alb" }
+  tags = {
+    Name = "smart-voting-alb"
+  }
 }
 
-# Target Group (unchanged)
 resource "aws_lb_target_group" "app" {
   name     = "smart-voting-tg"
   port     = 80
   protocol = "HTTP"
   vpc_id   = aws_vpc.main.id
+
   health_check {
     path                = "/"
     interval            = 30
@@ -20,13 +22,17 @@ resource "aws_lb_target_group" "app" {
     healthy_threshold   = 2
     unhealthy_threshold = 2
   }
-  tags = { Name = "smart-voting-tg" }
+
+  tags = {
+    Name = "smart-voting-tg"
+  }
 }
 
 resource "aws_lb_listener" "http" {
   load_balancer_arn = aws_lb.app.arn
   port              = "80"
   protocol          = "HTTP"
+
   default_action {
     type             = "forward"
     target_group_arn = aws_lb_target_group.app.arn
