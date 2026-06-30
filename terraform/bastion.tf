e728e82bad30d73b4aaf30195f94c92af0fc49d5
naveen saini <naveensaini6378@gmail.com>
2026-06-30 22:48:37 +0530

    fix bstion server

--- terraform/bastion.tf
+++ terraform/bastion.tf
@@ -22,9 +22,9 @@ data "aws_ami" "amazon_linux" {
 # Bastion Server
 resource "aws_instance" "bastion" {
   ami                    = data.aws_ami.amazon_linux.id
-  instance_type          = "t3.small"
+  instance_type          = "t4g.small"
   key_name               = "devops_ssh"
-  subnet_id              = aws_subnet.public[0].id
+  subnet_id              = aws_subnet.public[0].id.
   vpc_security_group_ids = [aws_security_group.bastion_sg.id]
 
   associate_public_ip_address = true
